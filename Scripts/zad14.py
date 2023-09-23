8a2ba3a8fab5ea8b944205a39c0a01ba026ea314
Полина Казанцева <143807265+kynween@users.noreply.github.com>
2023-09-23 13:06:27 +0700

    Update zad14.py

--- Scripts/zad14.py
+++ Scripts/zad14.py
@@ -1,12 +1,31 @@
-import threading
-import time
-from tkinter import messagebox
 
-def job():
-    
-    messagebox.showinfo('UsefulPython', 'отдохни брат')
+n = int(input("razmer"))
+mat = [[0]*n for i in range(n)]
+st, m = 1, 0
 
-while True:
-    time.sleep(1)
-    job()
-        
+mat[n//2][n//2]=n*n
+for v in range(n//2):
+
+    for i in range(n-m):
+        mat[v][i+v] = st
+        st+=1
+        #i+=1
+
+    for i in range(v+1, n-v):
+        mat[i][-v-1] = st
+        st+=1
+        #i+=1
+
+    for i in range(v+1, n-v):
+        mat[-v-1][-i-1] =st
+        st+=1
+        #i+=1
+
+    for i in range(v+1, n-(v+1)):
+        mat[-i-1][v]=st
+        st+=1
+
+    m+=2
+
+for i in mat:
+    print(*i)
